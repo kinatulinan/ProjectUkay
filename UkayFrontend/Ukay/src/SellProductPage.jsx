4023f807b13e29c7d4368c7a03fcd883cb876db1
Vargas, Kim B. <kim.vargas@cit.edu>
2024-11-01 13:12:18 +0800

    Display, Update, Delete at Product Listing

--- UkayFrontend/Ukay/src/SellProductPage.jsx
+++ UkayFrontend/Ukay/src/SellProductPage.jsx
@@ -1,43 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-
-function SellProductPage(){
-
-    return(
-        <>
-        
-        <div className='form-Design'>
-        <h1 className='sell-Title'>Sell A Product</h1>
-        <form action='SellProductPage.jsx'>
-            
-            <label className = 'label-adjust' for = "sellProductName">Product ID</label>
-            <input className = "Sell-Data" type="number" id="sellProductName" name="Product Name" placeholder="Product Name here"></input>
-
-            <label className = 'label-adjust' for = "sellProductType">Product Name</label>
-            <input className = "Sell-Data" classtype="text" id="sellProductType" name="Product Type" placeholder="Product Type here"></input>
-
-            <label className = 'label-adjust' for = "sellProductType">Product Type</label>
-            <input className = "Sell-Data" classtype="text" id="sellProductType" name="Product Type" placeholder="Product Type here"></input>
-
-            <label className = 'label-adjust' for = "sellProductPrice">Product Details</label>
-            <input className = "Sell-Data" type="text" id="sellProductPrice" name="Product Price" placeholder="Product Price here"></input>
-
-            <label className = 'label-adjust' for = "sellProductPrice">Product Price</label>
-            <input className = "Sell-Data" type="number" id="sellProductPrice" name="Product Price" placeholder="Product Price here"></input>
-
-            <label className = 'label-adjust' for = "sellProductPrice">Product Description</label>
-            <input className = "Sell-Data" type="text" id="sellProductPrice" name="Product Price" placeholder="Product Price here"></input>
-
-            <input className = "button-Submit" type="btnSubmit" value="Submit"></input>
-        </form>
-        </div>
-        <hr/>
-        
-        
-        </>
+import axios from 'axios';
 
 
-    );
+function SellProductPage() {
+  const [formData, setFormData] = useState({
+    sellProductName: '',
+    sellProductType: '',
+    sellProductPrice: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:8080/api/sell/post', formData);
+      console.log('Product added:', response.data);
+    } catch (error) {
+      console.error('Error adding product:', error);
+    }
+  };
+
+
+  return (
+    <div className='form-Design'>
+      <h1 className='sell-Title'>Sell A Product</h1>
+      <form onSubmit={handleSubmit}>
+        <label className='label-adjust' htmlFor='sellProductName'>Product Name</label>
+        <input
+          className='Sell-Data'
+          type='text'
+          id='sellProductName'
+          placeholder='Product Name here'
+          value={formData.sellProductName}
+          onChange={handleInputChange}
+        />
+
+        <label className='label-adjust' htmlFor='sellProductType'>Product Type</label>
+        <input
+          className='Sell-Data'
+          type='text'
+          id='sellProductType'
+          placeholder='Product Type here'
+          value={formData.sellProductType}
+          onChange={handleInputChange}
+        />
+
+
+        <label className='label-adjust' htmlFor='sellProductPrice'>Product Price</label>
+        <input
+          className='Sell-Data'
+          type='number'
+          id='sellProductPrice'
+          placeholder='Product Price here'
+          value={formData.sellProductPrice}
+          onChange={handleInputChange}
+        />
+
+        <button className='button-Submit' type='submit'>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default SellProductPage;
