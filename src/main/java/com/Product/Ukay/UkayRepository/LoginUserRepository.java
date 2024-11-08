926225fc0b0adcc70cec40a8cb3281a0ddd9fef8
immatchAA <143307155+immatchAA@users.noreply.github.com>
2024-11-08 13:45:46 +0800

    delete unnecessary files

--- src/main/java/com/Product/Ukay/UkayRepository/LoginUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Product.Ukay.UkayRepository;
-
-import com.Product.Ukay.UkayEntity.LoginUserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LoginUserRepository extends JpaRepository<LoginUserEntity, Integer> {
-}
