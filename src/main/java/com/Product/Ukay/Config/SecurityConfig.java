48573efd69e9b78645f1cae90a8181eec9c7880c
immatchAA <143307155+immatchAA@users.noreply.github.com>
2024-11-10 16:47:44 +0800

    updated SecurityConfig

--- src/main/java/com/Product/Ukay/Config/SecurityConfig.java
+++ src/main/java/com/Product/Ukay/Config/SecurityConfig.java
@@ -1,20 +1,17 @@
 package com.Product.Ukay.Config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig  {
-
+public class SecurityConfig {
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder () {
         return new BCryptPasswordEncoder();
     }
 
--- src/main/java/com/Product/Ukay/UkayService/AccountService.java
+++ src/main/java/com/Product/Ukay/UkayService/AccountService.java
@@ -4,8 +4,8 @@ import com.Product.Ukay.Dto.UserDto;
 import com.Product.Ukay.response.LoginResponse;
 
 public interface AccountService {
-   String addAccount(UserDto userDto);
+    String addAccount(UserDto userDto);
     LoginResponse loginUser(UserDto userDto);
 
-}
 
+}
