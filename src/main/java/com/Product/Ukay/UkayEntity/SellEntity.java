70fa18f270ce760eeba24da1ef52304c543340ba
kinatulinan <151549802+kinatulinan@users.noreply.github.com>
2024-11-11 22:28:25 +0800

    Comment test tutorial

--- src/main/java/com/Product/Ukay/UkayEntity/SellEntity.java
+++ src/main/java/com/Product/Ukay/UkayEntity/SellEntity.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class SellEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Test
     private int sellId;
 
     private String sellProductName;
