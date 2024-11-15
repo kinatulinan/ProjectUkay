f1933da0fca431ae1ae2e927fc0a9d2c50dcc6b9
inashi14 <150660177+inashi14@users.noreply.github.com>
2024-11-15 08:47:14 +0800

    Payment BE

--- src/main/java/com/Product/Ukay/UkayService/PaymentService.java
+++ src/main/java/com/Product/Ukay/UkayService/PaymentService.java
@@ -1,32 +1,12 @@
 package com.Product.Ukay.UkayService;
-
 import com.Product.Ukay.UkayEntity.PaymentEntity;
-import com.Product.Ukay.UkayRepository.PaymentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class PaymentService {
-
-    @Autowired //testing balik
-    private PaymentRepository pRepo;
-
-    public List<PaymentEntity> getAllPayments() {
-        return pRepo.findAll();
-    }
-
-    public PaymentEntity savePayment(PaymentEntity payment) {
-        return pRepo.save(payment);
-    }
-
-    public Optional<PaymentEntity> getPaymentById(int id) {
-        return pRepo.findById(id);
-    }
-
-    public void deletePayment(int id) {
-        pRepo.deleteById(id);
-    }
+public interface PaymentService {
+    List<PaymentEntity> getAllPayments();
+    PaymentEntity savePayment(PaymentEntity payment);
+    Optional<PaymentEntity> getPaymentById(int id);
+    void deletePayment(int id);
 }
--- src/main/java/com/Product/Ukay/UkayService/PaymentServicee.java
+++ src/main/java/com/Product/Ukay/UkayService/PaymentServicee.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class PaymentServicee {
+public class PaymentServicee implements PaymentService{
 
     @Autowired //testing balik
     private PaymentRepository pRepo;
